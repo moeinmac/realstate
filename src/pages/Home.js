8675ad4cadf0dac43bdfc2a82cc86f99821f6e7a
Moein.MAC <moein.mac@gmail.com>
2024-04-25 14:01:05 +0330

    search states

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,19 @@
 const Home = () => {
-  return <></>
-}
+  return (
+    <main>
+      <div className="flex gap-1 flex-col px-6 py-4">
+        <h1 className="font-kalameh text-4xl text-blue">مــــتراژ</h1>
+        <p className="font-alibaba">آگهی فروش مسکن</p>
+      </div>
+      <div className="px-6">
+        <input
+          className="bg-gray placeholder:text-[#dddeeb] text-black font-alibaba text-2xl outline-none px-4 py-2 rounded-lg w-full"
+          type="text"
+          placeholder="به دنبال چه میگردی؟"
+        />
+      </div>
+    </main>
+  );
+};
+
+export default Home;
