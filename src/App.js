ca8278076d207153a5a3e3b75918b5b5d6c3ffde
Moein.MAC <moein.mac@gmail.com>
2024-04-25 13:20:26 +0330

    Header Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Header from './components/Header/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
+    <Header />
+    <Main />
     </>
   );
 }
