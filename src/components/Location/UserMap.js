7ef5c94cde3375b78fa5aca4dab481d20732d74a
Moein.MAC <moein.mac@gmail.com>
2024-04-26 13:09:46 +0330

    estate item all done

--- src/components/Location/UserMap.js
+++ src/components/Location/UserMap.js
@@ -1,15 +1,20 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import LocationPicker from "./LocationPicker";
 
-const UserMap = ({ location }) => {
+const UserMap = ({ location, initLocation }) => {
   // dynamic init location #fix_later
   return (
-    <MapContainer center={[35.6892523, 51.3896004]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer
+      center={initLocation ? [initLocation.lat, initLocation.lng] : [35.6892523, 51.3896004]}
+      zoom={16}
+      scrollWheelZoom={true}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <LocationPicker location={location} />
+      {!location && <Marker position={[initLocation.lat, initLocation.lng]}></Marker>}
+      {location && <LocationPicker location={location} />}
     </MapContainer>
   );
 };
--- src/pages/Estate/EstateItem.js
+++ src/pages/Estate/EstateItem.js
@@ -1,43 +1,81 @@
 import { useLoaderData } from "react-router-dom";
 import { ImUserTie } from "react-icons/im";
 import { TbRulerMeasure } from "react-icons/tb";
+import { BiDetail } from "react-icons/bi";
+import { IoLocation } from "react-icons/io5";
+import { BsTelephone } from "react-icons/bs";
 import { persianNumbers } from "../../lib/persianNumbers";
 import { timeSince } from "../../lib/timeSince";
+import UserMap from "../../components/Location/UserMap";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 const EstateItem = () => {
   const response = useLoaderData();
+  const [showNumber, setShowNumber] = useState(false);
+  const showNumberHandler = () => setShowNumber(!showNumber);
+  const user = useSelector((state) => state.user);
+
   if (response.error) {
     return <h1>همچــنین ملکـــی وجود ندارد!</h1>;
   }
+
   const data = response.data;
-  console.log(data);
+
   return (
-    <>
+    <main className="relative">
       <header>
         <div>
           <img src={data.photo} className="max-w-full rounded-b-lg" />
         </div>
-        <div className="px-6 py-4 flex flex-col gap-2">
+        <div className="px-6 py-4 flex flex-col gap-4">
           <h1 className="font-kalameh text-4xl">{data.title}</h1>
-          <div className="flex items-center justify-between px-2 ">
+          <div className="flex items-center justify-between px-2 font-alibaba text-gray">
             <div className="flex items-center gap-2">
               <ImUserTie className="bg-green text-white text-2xl p-1 rounded-full" />
-              <p className="font-alibaba text-green">{data.user.fullname}</p>
+              <p>{data.user.fullname}</p>
             </div>
             <p>{timeSince(data.created_at)}</p>
           </div>
         </div>
       </header>
-      <article>
+      <article className="flex flex-col gap-4 px-6 pb-4">
         <div className="flex items-center gap-2">
-          <h3 className="font-kalameh text-2xl flex items-center">
+          <h3 className="font-kalameh text-2xl flex items-center gap-1">
             <TbRulerMeasure />
-            مــتراژ :{" "}
+            مــتراژ :
           </h3>
           <span className="font-alibaba">{persianNumbers(data.area)}</span>
         </div>
+        <div className={`flex gap-2 ${data.description ? "flex-col" : ""}`}>
+          <h3 className="font-kalameh text-2xl flex items-center gap-1">
+            <BiDetail />
+            توضیحات :
+          </h3>
+          <p className="font-alibaba">
+            {data.description ? data.description : "توضحیاتی جهت نمایش وجود ندارد"}
+          </p>
+        </div>
+        <div className="flex gap-2 flex-col">
+          <h3 className="font-kalameh text-2xl flex items-center gap-1">
+            <IoLocation />
+            موقعیت مکـانی :
+          </h3>
+          <div className="relative">
+            <UserMap initLocation={data.location} />
+          </div>
+        </div>
+        <button
+          onClick={showNumberHandler}
+          className="font-kalameh text-4xl flex  flex-col items-center bg-blue py-3  rounded-lg"
+        >
+          <div className="flex items-center gap-3">
+            {!showNumber && <BsTelephone />}
+            {!showNumber ? "شمــاره تـــماس" : user.isAuth ? data.user.phone : "ابتدا وارد  حساب تان شوید"}
+          </div>
+        </button>
       </article>
-    </>
+    </main>
   );
 };
 
